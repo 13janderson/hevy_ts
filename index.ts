1aa9ae593655338fee8698433aac6056f0dc8f55
Jack Anderson <jack.anderson@bjss.com>
2025-04-16 16:04:38 +0100

    pagination wip, needs some refactoring

--- index.ts
+++ index.ts
@@ -1,19 +1,29 @@
-import { v1Workouts, v1WorkoutsParams} from "./src/types/hevy-api-endpoints";
-import type { v1WorkoutsResponse } from "./src/types/hevy-api-endpoints";
+import { v1Workouts, v1WorkoutsParams, v1WorkoutsCount} from "./src/types/hevy-api-endpoints";
+import type { v1WorkoutsResponse, v1WorkoutsCountResponse} from "./src/types/hevy-api-endpoints";
 import type { Workout } from "./src/types/hevy-api-types";
 import * as dotenv from 'dotenv';
 
 // Loads .env file into process.env
 dotenv.config();
-async function GetWorkouts(): Promise<v1WorkoutsResponse | null> {
-    let v1 = new v1Workouts();
-    let api_key = process.env.API_KEY;
-    if (!api_key){
-        console.error("Failed to read API_KEY from .env.");
-        return null;
-    }
-    let params = new v1WorkoutsParams(api_key);
-    return v1.fetch(params);
-}
-    
 
+let api_key = process.env.API_KEY;
+if (!api_key){
+    throw new Error("Failed to read API_KEY from .env.");
+}
+
+let params = new v1WorkoutsParams(api_key)
+let workouts = new v1Workouts(params)
+
+var page = await(workouts.NextPage())
+while(page){
+    console.log((page?.workouts)?.length)
+    page = await (workouts.NextPage())
+
+    // console.log(page?.workouts[0])
+    // break
+}
+
+
+// let workoutsResponse = await GetAllWorkouts();
+// let workouts = workoutsResponse.workouts;
+// console.log(workouts[0].exercises[0].sets);
